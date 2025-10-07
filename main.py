bfe4458fb840647ff3f9fc412136c89a7269b1e8
RaihanK <raihank0192@gmail.com>
2025-10-07 07:48:14 +0600

    automation the web browser basic added here

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 # -------------- Day 48 --------------
+
+from selenium import webdriver
+
+# ----- keep Chrome browser open after program finishes ------------
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://www.amazon.com")
+
+
+# driver.close()    #for close single tab
+# driver.quit()   #for close all tab
+
+
